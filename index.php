d5b6ca35dcd40c6c26f62ce8b213400d22096a07
devdouglasa <douglasalisson27@gmail.com>
2024-08-31 11:14:10 -0300

    update: ajustando exibição de dados no HTML

--- index.php
+++ index.php
@@ -1,18 +1,27 @@
 <?php
 
 require __DIR__ . '/vendor/autoload.php';
-// require __DIR__ . '/src/MercadoPago.php';
 
 use App\Services\Connection;
 use App\Services\User;
 use App\MercadoPago;
+use Dotenv\Dotenv;
 
+## READ .ENV FILE
+$dotenv = Dotenv::createUnsafeImmutable(__DIR__);
+$dotenv->load();
 
+## API PAYMENT MERCADO PAGO
 $transaction = new MercadoPago();
 
 $payment = $transaction->paymentPreference();
 
-var_dump($payment);
+## VIEW HTML
+echo "<h1>Acesse o Link para realizar o pagamento!</h1>";
+
+echo "<button><a href='$payment'>Link de Pagamento</a></button>";
+
+// var_dump($payment);
 
 
 // $conn = new BancoDeDados();
